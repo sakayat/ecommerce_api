494e4d6c95921a9592d9d4b67f1533430a96598c
sakhyat <sakhyatkabir1@gmail.com>
2023-08-20 00:45:15 +0600

    added create product route

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,13 @@
+const Product = require("../models/productSchema")
+
 const productsRoute = (req,res) => {
     return res.json("hello")
 }
 
-module.exports = {productsRoute}
+const addProduct = async (req,res) => {
+    const body = await req.body
+    const product = await Product.create(body)
+    return res.json(product)
+}
+
+module.exports = {productsRoute,addProduct}
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,16 @@
-const { productsRoute } = require("../controllers/productsController");
 const express = require("express");
 const router = express.Router();
 
+const {
+  productsRoute,
+  addProduct,
+} = require("../controllers/productsController");
+
 // get route
 router.get("/products", productsRoute);
+
+// post route
+
+router.post("/add/product", addProduct);
 
 module.exports = router;

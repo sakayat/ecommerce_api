d4d18ee63d40d6b6182ec96f80ae1cc81da2dc4e
sakhyat <sakhyatkabir1@gmail.com>
2023-08-20 00:55:10 +0600

    updated get and add route

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,22 @@
-const Product = require("../models/productSchema")
+const Product = require("../models/productSchema");
 
-const productsRoute = (req,res) => {
-    return res.json("hello")
-}
+const productsRoute = async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res.json(products);
+  } catch (error) {
+    return res.json({ message: "internal server error" });
+  }
+};
 
-const addProduct = async (req,res) => {
-    const body = await req.body
-    const product = await Product.create(body)
-    return res.json(product)
-}
+const addProduct = async (req, res) => {
+  const body = await req.body;
+  try {
+    const product = await Product.create(body);
+    return res.json(product);
+  } catch (error) {
+    return res.json({ message: "internal server error" });
+  }
+};
 
-module.exports = {productsRoute,addProduct}
+module.exports = { productsRoute, addProduct };
